a78f4d02d20b305b2b2f701fe66e1bc686fee0a7
saggers94 <sagarparmar0233@gmail.com>
2024-10-16 19:42:58 -0500

    Added calc and geometry

--- geometry/cube.go
+++ geometry/cube.go
@@ -3,3 +3,14 @@ package geometry
 func cubeVolume(n int) int {
 	return n * n * n
 }
+
+// go utilizes semantic versioning
+// Numbering scheme used by gomodule
+// vMAJOR.MINOR.PATCH
+// v1.2.3 // Major version 1, Minor version 2, Patch version 3
+// Major means breaking changes or when you make incompatible changes
+// Minor version means to add functionality in a backwards-compatible manner
+// Patch version means to make backwards-compatible bug fixes
+
+// With this command you can give a specific version to the package
+// git tag -a v1.0.0 -m "initial release"
